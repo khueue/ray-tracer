834ccb1e47f743ee6799b2f4254c86c19d26a5ce
Sebastian Lundström <khueue@gmail.com>
2019-06-23 18:47:42 +0200

    Matrix multiplication

--- app/src/js/lib/matrices.js
+++ app/src/js/lib/matrices.js
@@ -1,4 +1,41 @@
 const numbers = require('./numbers');
+const tuples = require('./tuples');
+
+/**
+ * General purpose matrix constructor supporting two forms:
+ *
+ * Literal:
+ * - Matrix([[...], ...])
+ *
+ * Dimensions and init value:
+ * - Matrix(4, 1, 0.0)
+ *
+ * For efficiency, use the MatrixXX functions where applicable.
+ */
+function Matrix(rows, cols, init = NaN) {
+	if (typeof rows === 'object') {
+		// Assume it's an Array representing a matrix and just return it.
+		// Note that this does not make a copy.
+		return rows;
+	} else {
+		// Construct new matrix from dimensions and init value.
+		let m = new Array(rows);
+		for (let row = 0; row < rows; ++row) {
+			m[row] = new Array(cols).fill(init);
+		}
+		return m;
+	}
+}
+
+// Currently only supports 4x4.
+// function MatrixZero() {
+// 	return [
+// 		[0.0, 0.0, 0.0, 0.0],
+// 		[0.0, 0.0, 0.0, 0.0],
+// 		[0.0, 0.0, 0.0, 0.0],
+// 		[0.0, 0.0, 0.0, 0.0],
+// 	];
+// }
 
 function Matrix22(m) {
 	// Note that this will not create new objects.
@@ -19,8 +56,10 @@ function equal(a, b) {
 	if (a.length !== b.length) {
 		return false;
 	} else {
-		for (let row = 0; row < a.length; ++row) {
-			for (let col = 0; col < a[row].length; ++col) {
+		const rows = a.length;
+		const cols = a[0].length;
+		for (let row = 0; row < rows; ++row) {
+			for (let col = 0; col < cols; ++col) {
 				if (!numbers.equal(a[row][col], b[row][col])) {
 					return false;
 				}
@@ -30,8 +69,49 @@ function equal(a, b) {
 	}
 }
 
-function valueAt(m, row, col) {
-	return m[row][col];
+/**
+ * Only supports 4-col x 4-row matrices.
+ */
+function mult(a, b) {
+	const rows = a.length; // Rows of a.
+	const cols = b[0].length; // Cols of b.
+
+	let m = Matrix(rows, cols, 0.0);
+
+	for (let row = 0; row < rows; ++row) {
+		for (let col = 0; col < cols; ++col) {
+			m[row][col] =
+				a[row][0] * b[0][col] +
+				a[row][1] * b[1][col] +
+				a[row][2] * b[2][col] +
+				a[row][3] * b[3][col];
+		}
+	}
+
+	return m;
+}
+
+function tupleAsMatrix(t) {
+	// prettier-ignore
+	return Matrix([
+		[t.x],
+		[t.y],
+		[t.z],
+		[t.w],
+	]);
+}
+
+/**
+ * Only supports 4x1 matrices.
+ */
+function matrixAsTuple(m) {
+	return tuples.Tuple(m[0][0], m[1][0], m[2][0], m[3][0]);
+}
+
+function multTuple(a, t) {
+	const b = tupleAsMatrix(t);
+	const m = mult(a, b);
+	return matrixAsTuple(m);
 }
 
 module.exports = {
@@ -39,5 +119,6 @@ module.exports = {
 	Matrix33,
 	Matrix44,
 	equal,
-	valueAt,
+	mult,
+	multTuple,
 };
--- app/src/js/lib/matrices.test.js
+++ app/src/js/lib/matrices.test.js
@@ -2,6 +2,7 @@ const test = require('tap').test;
 
 const numbers = require('./numbers');
 const matrices = require('./matrices');
+const tuples = require('./tuples');
 
 test('construct 2x2 matrix', function(t) {
 	// prettier-ignore
@@ -10,10 +11,10 @@ test('construct 2x2 matrix', function(t) {
 		[1.0, -2.0],
 	]);
 
-	t.ok(numbers.equal(matrices.valueAt(m, 0, 0), -3.0));
-	t.ok(numbers.equal(matrices.valueAt(m, 0, 1), 5.0));
-	t.ok(numbers.equal(matrices.valueAt(m, 1, 0), 1.0));
-	t.ok(numbers.equal(matrices.valueAt(m, 1, 1), -2.0));
+	t.ok(numbers.equal(m[0][0], -3.0));
+	t.ok(numbers.equal(m[0][1], 5.0));
+	t.ok(numbers.equal(m[1][0], 1.0));
+	t.ok(numbers.equal(m[1][1], -2.0));
 
 	t.end();
 });
@@ -25,9 +26,9 @@ test('construct 3x3 matrix', function(t) {
 		[0.0, 1.0, 1.0],
 	]);
 
-	t.ok(numbers.equal(matrices.valueAt(m, 0, 0), -3.0));
-	t.ok(numbers.equal(matrices.valueAt(m, 1, 1), -2.0));
-	t.ok(numbers.equal(matrices.valueAt(m, 2, 2), 1.0));
+	t.ok(numbers.equal(m[0][0], -3.0));
+	t.ok(numbers.equal(m[1][1], -2.0));
+	t.ok(numbers.equal(m[2][2], 1.0));
 
 	t.end();
 });
@@ -40,13 +41,13 @@ test('construct 4x4 matrix', function(t) {
 		[13.5, 14.5, 15.5, 16.5],
 	]);
 
-	t.ok(numbers.equal(matrices.valueAt(m, 0, 0), 1.0));
-	t.ok(numbers.equal(matrices.valueAt(m, 0, 3), 4.0));
-	t.ok(numbers.equal(matrices.valueAt(m, 1, 0), 5.5));
-	t.ok(numbers.equal(matrices.valueAt(m, 1, 2), 7.5));
-	t.ok(numbers.equal(matrices.valueAt(m, 2, 2), 11.0));
-	t.ok(numbers.equal(matrices.valueAt(m, 3, 0), 13.5));
-	t.ok(numbers.equal(matrices.valueAt(m, 3, 2), 15.5));
+	t.ok(numbers.equal(m[0][0], 1.0));
+	t.ok(numbers.equal(m[0][3], 4.0));
+	t.ok(numbers.equal(m[1][0], 5.5));
+	t.ok(numbers.equal(m[1][2], 7.5));
+	t.ok(numbers.equal(m[2][2], 11.0));
+	t.ok(numbers.equal(m[3][0], 13.5));
+	t.ok(numbers.equal(m[3][2], 15.5));
 
 	t.end();
 });
@@ -99,10 +100,56 @@ test('different size matrices', function(t) {
 		[1.0, 2.0, 3.0, 4.0],
 		[5.5, 6.5, 7.5, 8.5],
 		[9.0, 10.0, 11.0, 12.0],
-		[13.5, 14.5, 15.5, 17.5], // Diff at last.
+		[13.5, 14.5, 15.5, 17.5],
 	]);
 
 	t.ok(!matrices.equal(a, b));
+
+	t.end();
+});
+
+test('matrix mult, 4x4', function(t) {
+	const a = matrices.Matrix44([
+		[1.0, 2.0, 3.0, 4.0],
+		[5.0, 6.0, 7.0, 8.0],
+		[9.0, 8.0, 7.0, 6.0],
+		[5.0, 4.0, 3.0, 2.0],
+	]);
+	const b = matrices.Matrix44([
+		[-2.0, 1.0, 2.0, 3.0],
+		[3.0, 2.0, 1.0, -1.0],
+		[4.0, 3.0, 6.0, 5.0],
+		[1.0, 2.0, 7.0, 8.0],
+	]);
+
+	const actual = matrices.mult(a, b);
+
+	const expected = matrices.Matrix44([
+		[20.0, 22.0, 50.0, 48.0],
+		[44.0, 54.0, 114.0, 108.0],
+		[40.0, 58.0, 110.0, 102.0],
+		[16.0, 26.0, 46.0, 42.0],
+	]);
+
+	t.ok(matrices.equal(actual, expected));
+
+	t.end();
+});
+
+test('matrix mult, by tuple', function(t) {
+	const m = matrices.Matrix44([
+		[1.0, 2.0, 3.0, 4.0],
+		[2.0, 4.0, 4.0, 2.0],
+		[8.0, 6.0, 4.0, 1.0],
+		[0.0, 0.0, 0.0, 1.0],
+	]);
+	const tup = tuples.Tuple(1.0, 2.0, 3.0, 1.0);
+
+	const actual = matrices.multTuple(m, tup);
+
+	const expected = tuples.Tuple(18.0, 24.0, 33.0, 1.0);
+
+	t.ok(tuples.equal(actual, expected));
 
 	t.end();
 });
